5229b7fec85c4d492e7cf642e05cbcd6ba5a5f14
Moses-247 <asugam9@gmail.com>
2025-02-07 11:43:56 +0300

    Schema

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 require('dotenv').config();
+
+const Person = require('./person');
 const mongoose = require('mongoose');
 
+
+//Create a Person Instance and Save:
+const createAndSavePerson = () =>{
+    const person = new Person({
+        name:"Moses Asuga",
+        age: 24,
+        favoriteFoods:['Chapo','samosa']
+    });
+
+    person.save((err, data) =>{
+        if(err) return console.error(err);
+        console.log('Person saved:', data);
+    });
+};
+
+createAndSavePerson();
+
+
+//Connecting to Database
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
--- person.js
+++ person.js
@@ -1,3 +1,4 @@
+//Defining Schema
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
